2d8df132a88c11be8af77593f2439d7bdda2bdde
Rakesh Mahadasa <rakeshnitcalicut@gmail.com>
2019-02-07 17:25:30 -0800

    Flush Analysis Added

--- runner.py
+++ runner.py
@@ -1,27 +1,43 @@
 import os
-block_iter_map = {
-    1024 : 2000000,
-    2048 : 1000000,
-    4096 : 500000,
-    8192 : 250000,
-    16384 : 125000,
-    32768 : 62500,
-    65536 : 31250 ,
-    1024000 : 2000,
-    2048000 : 1000,
-    4096000 : 500,
-    8192000 : 250
-}
-def writer():
+block_iter_map = [
+[1024 , 2000000],
+[2048 , 1000000],
+[4096 , 500000],
+[8192 , 250000],
+[16384 , 125000],
+[32768 , 62500],
+[65536 , 31250 ],
+[1024000 , 2000],
+[2048000 , 1000],
+[4096000 , 500],
+[8192000 , 250]
+]
+stat_files = ["stats/unif_data_stats.out","stats/unif_iter_stats.out","stats/flush_writer_stats.out","stats/reader_stats.out"]
+for file in stat_files:
+        open(file, 'w').close()
+
+def unif_data_writer():
     os.system("g++ -std=c++11 writer.cpp -o writer")
     for x in block_iter_map:
-        os.system("./writer "+str(x)+" "+str(1000)+" >> "+"stats.out")
+        os.system("./writer "+str(x[0])+" "+str(x[1])+" 1 >> "+"stats/unif_data_stats.out")
+
+def unif_iter_writer():
+    os.system("g++ -std=c++11 writer.cpp -o writer")
+    for x in block_iter_map:
+        os.system("./writer "+str(x[0])+" "+str(1000)+" 2 >> "+"stats/unif_iter_stats.out")
+
+def flush_writer():
+    os.system("g++ -std=c++11 writer.cpp -o writer")
+    for x in block_iter_map:
+        os.system("./writer "+str(x[0])+" "+str(1000)+" 3 >> "+"stats/flush_writer_stats.out")
 
 def reader():
     os.system("g++ -std=c++11 reader.cpp -o reader")
     for x in block_iter_map:
-        os.system("./reader "+str(x)+" "+str(1000)+" >> "+"stats.out")
+        os.system("./reader "+str(x[0])+" "+str(1000)+" >> "+"stats/reader_stats.out")
 
 if __name__ == "__main__":
-    #writer()
+    unif_data_writer()
+    unif_iter_writer()
+    flush_writer()
     reader()
--- writer.cpp
+++ writer.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 using namespace std::chrono;
-
+char block[134217728];
 int main(int argc,char** argv){
     
     if(argc < 2){
@@ -20,28 +20,45 @@ int main(int argc,char** argv){
 
     uint32_t block_size = atoi(argv[1]);
     uint32_t iter_count = atoi(argv[2]);
+    uint16_t option = atoi(argv[3]);
 
-    char block[block_size];
+    
     memset(block,'0',sizeof(block));
     string out_file = "out/"+to_string(block_size)+".bin";
     FILE* ofile = fopen(out_file.c_str(),"w+b");
     fsync(fileno(ofile));
-    vector<uint64_t> runtimes;
-    for(int i=0;i<iter_count;i++){
-        high_resolution_clock::time_point start_time = high_resolution_clock::now();
-        fwrite(&block,sizeof(block),1,ofile);
-        high_resolution_clock::time_point end_time = high_resolution_clock::now();
-        auto time_diff = duration_cast<milliseconds> (end_time - start_time).count();
-        runtimes.push_back(time_diff);
+    vector<uint64_t> runtimes(2000,0);
+    if(option == 3){
+        for (int i = 0; i < iter_count; i++)
+        {
+            high_resolution_clock::time_point start_time = high_resolution_clock::now();
+            fwrite(&block, sizeof(block), 1, ofile);
+            fsync(fileno(ofile));
+            high_resolution_clock::time_point end_time = high_resolution_clock::now();
+            auto time_diff = duration_cast<milliseconds>(end_time - start_time).count();
+            runtimes[i]=time_diff;
+        }
     }
+    else{
+        for (int i = 0; i < iter_count; i++)
+        {
+            high_resolution_clock::time_point start_time = high_resolution_clock::now();
+            fwrite(&block, sizeof(block), 1, ofile);
+            if(i==iter_count) fsync(fileno(ofile));
+            high_resolution_clock::time_point end_time = high_resolution_clock::now();
+            auto time_diff = duration_cast<milliseconds>(end_time - start_time).count();
+            runtimes.push_back(time_diff);
+        }
+    }
+    
     double sum = accumulate(begin(runtimes), end(runtimes), 0.0);
-    double mean =  sum / runtimes.size();
-
+    double mean =  sum / iter_count;
+    cout<<sum<<" "<<runtimes.size()<<endl;
     double sq_sum = 0.0;
     for(vector<uint64_t>::iterator it = runtimes.begin();it!=runtimes.end();++it) {
         sq_sum += (*it - mean) * (*it - mean);
     }
-    double stdev = sqrt(sq_sum / (runtimes.size()-1));
+    double stdev = sqrt(sq_sum / (iter_count-1));
     cout<<block_size<<" "<<sum<<" "<<mean<<" "<<stdev<<endl;
     fsync(fileno(ofile));
     fclose(ofile);
