be3990fc7ccfe940d5550a34b7bfdc65882ca054
Rakesh Mahadasa <rakeshnitcalicut@gmail.com>
2019-02-20 15:48:09 -0800

    Parallel Writer Added

--- parallel_writer.cpp
+++ parallel_writer.cpp
@@ -11,101 +11,34 @@
 using namespace std;
 using namespace std::chrono;
 
-int NUM_THREADS = 8;
-int wait_counter = 0;
-#ifndef QUEUE_SIZE
-#define QUEUE_SIZE 8
-#endif
+int NUM_THREADS = 0;
+std::vector<std::vector<uint64_t> > t_runtimes(8,std::vector<uint64_t>(50)); 
+int r_idx=0;
+char block[268435456];
+int block_size=256;
+int writers_per_thread = 0;
+pthread_mutex_t mtx;
 
-template <class ET>
-inline bool CAS(ET *ptr, ET oldv, ET newv) {
-  if (sizeof(ET) == 1) {
-    return __sync_bool_compare_and_swap((bool*)ptr, *((bool*)&oldv), *((bool*)&newv));
-  } else if (sizeof(ET) == 4) {
-    return __sync_bool_compare_and_swap((int*)ptr, *((int*)&oldv), *((int*)&newv));
-  } else if (sizeof(ET) == 8) {
-    return __sync_bool_compare_and_swap((long*)ptr, *((long*)&oldv), *((long*)&newv));
-  }
-  else {
-    std::cout << "CAS bad length : " << sizeof(ET) << std::endl;
-    abort();
-  }
-}
+void* parallel_write(void* input_args){
+    
+    int tid = *((int *) input_args);
+    string out_file = "out/"+to_string(block_size)+ "/" +to_string(tid) +".bin";
+    FILE* ofile = fopen(out_file.c_str(),"w+b");
+    fsync(fileno(ofile));
 
-template <class ET>
-inline ET writeAdd(ET *a, ET b) {
-  volatile ET newV, oldV;
-  do {oldV = *a; newV = oldV + b;}
-  while (!CAS(a, oldV, newV));
-  return newV;
-}
-
-
-class bqueue
-{
-  private:
-    char **worklist;
-    int start;
-    int pivot;
-    int end;
-
-  public:
-    bqueue()
-    {
-        worklist = new char*[QUEUE_SIZE];
-        for(int i = 0; i < QUEUE_SIZE; i++){
-            worklist[i] = new char[50331648];
-            memset(worklist[i],'0',sizeof(worklist[i]));
-        }
-        start = 0;
-        pivot=0;
-        end = 0;
-    };
-
-    void give_work()
-    {
-        int curr_end = writeAdd(&end,1);
-        while (!CAS(&pivot, curr_end-1, curr_end ))
-            sched_yield();
+    for (int i = 0; i < writers_per_thread; i++)
+    {   
+        
+        high_resolution_clock::time_point start_time = high_resolution_clock::now();
+        fwrite(&block, sizeof(block), 1, ofile);
+        fsync(fileno(ofile));
+        high_resolution_clock::time_point end_time = high_resolution_clock::now();
+        auto time_diff = duration_cast<milliseconds>(end_time - start_time).count();
+        t_runtimes[tid][i] = time_diff;
     }
-
-    int take_work(int waitline)
-    {
-        while (true)
-        {
-            int curr_start = start;
-            int curr_pivot = pivot;
-            if (curr_start == curr_pivot)
-            {  
-                if(waitline)
-                    writeAdd(&wait_counter,1);
-                if(wait_counter == NUM_THREADS){
-                    for(int i=0;i<NUM_THREADS-1;i++){                        
-                        give_work();
-                    }
-                    return -1;
-                }
-                return -2;
-            }
-            if(!waitline){
-                writeAdd(&wait_counter,-1);
-                waitline=true;
-            }
-            int args = worklist[curr_start];
-            if (CAS(&start, curr_start, curr_start + 1))
-            {
-                return args;
-            }
-        }
-    }
-
-    ~bqueue()
-    {
-
-        delete[] worklist;
-    };
-};
-
+    fclose(ofile);
+    pthread_exit(0);
+}   
 
 int main(int argc,char** argv){
     
@@ -117,33 +50,38 @@ int main(int argc,char** argv){
 
     uint32_t block_size = atoi(argv[1]);
     uint32_t iter_count = atoi(argv[2]);
-    uint16_t option = atoi(argv[3]);
-
-    
+    NUM_THREADS = atoi(argv[3]);
+    writers_per_thread = iter_count / NUM_THREADS;
     memset(block,'0',sizeof(block));
-    string out_file = "out/"+to_string(block_size)+".bin";
-    FILE* ofile = fopen(out_file.c_str(),"w+b");
-    fsync(fileno(ofile));
-    vector<uint64_t> runtimes(2000,0);
-    for (int i = 0; i < iter_count; i++)
+    pthread_t threads[NUM_THREADS];
+    high_resolution_clock::time_point t_start_time = high_resolution_clock::now();
+    for(int i = 0; i < NUM_THREADS; i++){
+        int rc =  pthread_create(&threads[i],NULL,parallel_write,new int(i));
+        if( rc  != 0 ){
+            cout<<"Thread creation Failed : "<<i<<endl;
+            exit(1);
+        }
+    }
+    for(int i = 0; i < NUM_THREADS; i++){
+        pthread_join(threads[i],NULL);
+    }
+    high_resolution_clock::time_point t_end_time = high_resolution_clock::now();
+    cout<<"Total Run Time : "<<duration_cast<milliseconds>(t_end_time - t_start_time).count()<<endl;
+    cout<<"======Per Thread Stats========="<<endl;
+    for (int i = 0; i < NUM_THREADS; i++)
     {
-        high_resolution_clock::time_point start_time = high_resolution_clock::now();
-        fwrite(&block, sizeof(block), 1, ofile);
-        fsync(fileno(ofile));
-        high_resolution_clock::time_point end_time = high_resolution_clock::now();
-        auto time_diff = duration_cast<milliseconds>(end_time - start_time).count();
-        runtimes[i] = time_diff;
+        cout<<"TID : "<<i<<endl;
+        double sum = accumulate(begin(t_runtimes[i]), end(t_runtimes[i]), 0.0);
+        double mean = sum / writers_per_thread;
+        double sq_sum = 0.0;
+        for (vector<uint64_t>::iterator it = t_runtimes[i].begin(); it != t_runtimes[i].end(); ++it)
+        {
+            sq_sum += (*it - mean) * (*it - mean);
+        }
+        double stdev = sqrt(sq_sum / (writers_per_thread - 1));
+        cout << sum <<" "<< mean << " " << stdev << endl;
     }
-    double sum = accumulate(begin(runtimes), end(runtimes), 0.0);
-    double mean =  sum / iter_count;
-    cout<<sum<<" "<<runtimes.size()<<endl;
-    double sq_sum = 0.0;
-    for(vector<uint64_t>::iterator it = runtimes.begin();it!=runtimes.end();++it) {
-        sq_sum += (*it - mean) * (*it - mean);
-    }
-    double stdev = sqrt(sq_sum / (iter_count-1));
-    fsync(fileno(ofile));
-    fclose(ofile);
-    return 0;
+    
+    pthread_exit(0);
 
 }
